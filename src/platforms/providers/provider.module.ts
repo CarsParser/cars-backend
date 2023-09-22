7c4981792f9f3e5048c19a643f4eeef604189fd7
Vladimir Gololobov <Vladimir.Gololobov@x5.ru>
2023-09-22 22:36:27 +0300

    fix(): import fix

--- src/platforms/providers/provider.module.ts
+++ src/platforms/providers/provider.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AvitoModule } from './avito/avito.module';
 import { ProviderFactory } from './provider.factory';
-import { ProxyModule } from 'src/proxy/proxy.module';
 
 @Module({
   imports: [AvitoModule],
