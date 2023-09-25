5479cb6bd2a7f5a6838a34a791ebd2b172eda3af
Vladimir Gololobov <Vladimir.Gololobov@x5.ru>
2023-09-25 22:25:01 +0300

    fix(): partitons fix

--- src/car/car.module.ts
+++ src/car/car.module.ts
@@ -10,6 +10,7 @@ import { CarConsumerController } from './car.consumer.controller';
 import { PlatformsModule } from 'src/platforms/platforms.module';
 import { BlockerService } from 'src/helpers/blocker.service';
 import { ProxyModule } from 'src/proxy/proxy.module';
+import { City } from 'src/common';
 
 @Module({
   imports: [
@@ -30,6 +31,44 @@ import { ProxyModule } from 'src/proxy/proxy.module';
         return ClientProxyFactory.create({
           transport: Transport.KAFKA,
           options: {
+            producer: {
+              createPartitioner:
+                () =>
+                ({ topic, message }) => {
+                  if (topic === 'load_cars') {
+                    const data = JSON.parse(message.value as string);
+
+                    switch (data.city as City) {
+                      case City.spb: {
+                        return 0;
+                      }
+                      case City.arkh: {
+                        return 1;
+                      }
+                      case City.ekb: {
+                        return 2;
+                      }
+                      case City.kazan: {
+                        return 3;
+                      }
+                      case City.msk: {
+                        return 4;
+                      }
+                      case City.omsk: {
+                        return 5;
+                      }
+                      case City.rostov: {
+                        return 6;
+                      }
+                      case City.samara: {
+                        return 7;
+                      }
+                    }
+                  }
+
+                  return 0;
+                },
+            },
             client: {
               clientId: 'cars',
               brokers: [
